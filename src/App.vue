f08e4b8dbe5a286d1135a1f28db58e94f6535328
margaux-works <margaux.espinasse@gmail.com>
2025-03-04 16:18:01 +0100

    rebase: remove exposed API kex and resolve app.vue conflict eather component, lat lon added

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import Weather from './components/Weather.vue';
+</script>
+
+<template>
+  <div class="h-screen flex items-center justify-center bg-gray-200">
+    <h1 class="text-4xl font-bold text-red-500">Tailwind is working! 🚀</h1>
+  </div>
+  <Weather />
+</template>
+
+<style scoped></style>
