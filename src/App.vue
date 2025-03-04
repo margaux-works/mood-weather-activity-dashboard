e20079f7d65f0e3f0497f6e71074e71fb1560c40
margaux-works <margaux.espinasse@gmail.com>
2025-03-04 16:18:01 +0100

    weather component, lat lon added

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,12 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import Weather from './components/Weather.vue';
 </script>
 
 <template>
   <div class="h-screen flex items-center justify-center bg-gray-200">
     <h1 class="text-4xl font-bold text-red-500">Tailwind is working! 🚀</h1>
   </div>
+  <Weather />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
---
+++ src/components/Weather.vue
@@ -0,0 +1,23 @@
+<script>
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+function success(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+
+  console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+}
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+}
+
+navigator.geolocation.getCurrentPosition(success, error);
+
+const apiUrl =
+  'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={apiKey}';
+</script>
+
+<template></template>
+
+<style></style>
